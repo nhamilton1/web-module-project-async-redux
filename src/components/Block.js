61b24d1f561f336637cd4f04555cd516fc61f81c
nhamilton1 <nickh3592@gmail.com>
2021-09-16 14:06:07 -0400

    moment removed

--- src/components/Block.js
+++ src/components/Block.js
@@ -5,7 +5,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import moment from 'moment'
 
 
 const useStyles = makeStyles({
@@ -24,6 +23,7 @@ const Block = (props) => {
 
     useEffect(() => {
         props.getBlock()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     if (error) {
